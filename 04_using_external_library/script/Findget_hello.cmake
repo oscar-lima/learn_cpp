d0462d50adbf29ee2a80a3d5f30ed636e0ae39d6
Oscar Lima <olima_84@yahoo.com>
2013-11-17 19:36:16 +0100

    renaming files to be more meaningfull

--- 04_using_external_library/script/Findget_hello.cmake
+++ 04_using_external_library/script/Findget_hello.cmake
@@ -2,9 +2,9 @@
 
 #find_library(get_hello_LIBRARIES namer /usr/lib "$ENV{NAMER_ROOT}")
 
-find_path(get_hello_INCLUDE_DIRS get_hello.h /home/oscar/Documents/c++/03_building_a_library_2/include/)
+find_path(get_hello_INCLUDE_DIRS get_hello.h /home/oscar/Documents/c++/03_building_a_library/include/)
 
-find_library(get_hello_LIBRARIES get_hello /home/oscar/Documents/c++/03_building_a_library_2/build/)
+find_library(get_hello_LIBRARIES get_hello /home/oscar/Documents/c++/03_building_a_library/build/)
 
 set(get_hello_FOUND TRUE)
  
