df854087c026b491879ba1ce4211a30f1feb3b6e
Oscar Lima <olima_84@yahoo.com>
2019-12-15 16:21:38 +0100

    add enum

---
+++ 05_many_cpp_functionality/enum/src/enum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "enum example" << endl;
+    
+    enum
+    {
+        a, b, c
+    };
+    
+    cout << a << b << c << endl;
+    
+}
